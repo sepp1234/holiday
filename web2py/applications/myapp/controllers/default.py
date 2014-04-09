b6de92dbbfa09f3a6a4bd0773c101608c1f7e12b
sepp1234 <s.knittel@gmx.net>
2014-04-09 13:38:22 +0200

    web2py added

---
+++ web2py/applications/myapp/controllers/default.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+
+def index():
+    session.counter = (session.counter or 0) + 1
+    return dict (message="Web2py Stinkt!", counter=session.counter)
